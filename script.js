f269f62ddbbdc3a27470b5427531aa8af9eea298
Pablo <senapablo@gmail.com>
2020-11-12 04:54:27 -0300

    Proyecto finalizado sin card hover

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-window.addEventListener("scroll", function() {
-    var header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0) ;
-})
+window.addEventListener('scroll', function() {
+    var header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0) ;
+});
+
+
+document.querySelector(".menu-icon").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+  });
+
+  ScrollReveal().reveal('.cards', {delay:400});
+  ScrollReveal().reveal('.footer', {delay:400});
+  ScrollReveal().reveal('.banner', {delay:400});
